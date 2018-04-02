3b711016a759e548c898c254c167c15b22898492
Dmitry Kruhlov <dmitriy.kruglov.work@gmail.com>
2018-04-02 16:21:16 +0300

    Added simple get methods for song and relation

--- webplayer/model/relation.py
+++ webplayer/model/relation.py
@@ -11,14 +11,27 @@ from webplayer.model import DeclarativeBase, metadata, DBSession
 class Relation(DeclarativeBase):
     __tablename__ = 'relations'
 
-    left_uid = Column(Integer, primary_key=True)
-    right_uid = Column(Integer, primary_key=True)
+    left_song_id = Column(Integer, primary_key=True)
     weight = Column(Integer, nullable=False)
+    right_song_id = Column(Integer, primary_key=True)
 
     user_id = Column(Integer, ForeignKey('tg_user.user_id'), index=True)
     user = relationship('User', uselist=False,
                         backref=backref('relations',
                                         cascade='all, delete-orphan'))
 
+    def __repr__(self):
+        return'<Relation: left=%s, weight=%s, right=%s>' % (
+            repr(self.left_song_id),
+            repr(self.weight),
+            repr(self.right_song_id))
+
+    @classmethod
+    def get_by_left(cls, id):
+        return DBSession.query(cls).filter_by(left_song_id=id).first()
+
+    @classmethod
+    def get_by_right(cls, id):
+        return DBSession.query(cls).filter_by(right_song_id=id).first()
 
 __all__ = ['Relation']
--- webplayer/model/song.py
+++ webplayer/model/song.py
@@ -11,13 +11,22 @@ from webplayer.model import DeclarativeBase, metadata, DBSession
 class Song(DeclarativeBase):
     __tablename__ = 'songs'
 
-    uid = Column(Integer, primary_key=True)
+    song_id = Column(Integer, primary_key=True)
     data = Column(Unicode(255), nullable=False)
+
 
     user_id = Column(Integer, ForeignKey('tg_user.user_id'), index=True)
     user = relationship('User', uselist=False,
                         backref=backref('songs',
                                         cascade='all, delete-orphan'))
 
+    def __repr__(self):
+    	return '<Song: name=%s, data=%s>' % (
+    		repr(self.song_id),
+    		repr(self.data))
+
+    @classmethod
+    def get(cls, song_id):
+    	return DBSession.query(cls).filter_by(song_id=song_id).first()
 
 __all__ = ['Song']
