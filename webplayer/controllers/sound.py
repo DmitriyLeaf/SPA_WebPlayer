e85c2b56d4d60753e6faaf64542d38343fa77d32
Dmitry Kruhlov <dmitriy.kruglov.work@gmail.com>
2018-04-11 10:02:01 +0300

    Updated sound model, controller, template

--- webplayer/controllers/sound.py
+++ webplayer/controllers/sound.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
-"""Song controller module"""
+"""Sound controller module"""
 
-from tg import expose, redirect, validate, flash, url
+from tg import expose, redirect, validate, flash, url, tmpl_context
 # from tg.i18n import ugettext as _
-# from tg import predicates
+from tg import predicates
 
 from webplayer.lib.base import BaseController
-# from webplayer.model import DBSession
+from webplayer.model import DBSession
+from webplayer.model.sound import Sound
 
+__all__ = ['SoundController']
 
-class SongController(BaseController):
+class SoundController(BaseController):
     allow_only = predicates.not_anonymous()
+
+    def _before(self, *args, **kw):
+        tmpl_context.project_name = "SpacePlayer"
     
     @expose('webplayer.templates.sound')
-    def index(self, **kw):
-        return dict(page='sound-index')
+    def index(self):
+    	#sound_test = DBSession.query(Sound).get(1)
+        return dict(page='sound')
+        	#sound=sound_test)
--- webplayer/model/sound.py
+++ webplayer/model/sound.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Song model module."""
+"""Sound model module."""
 from sqlalchemy import *
 from sqlalchemy import Table, ForeignKey, Column
 from sqlalchemy.types import Integer, Unicode, DateTime, LargeBinary
@@ -13,7 +13,6 @@ class Sound(DeclarativeBase):
 
     sound_id = Column(Integer, primary_key=True)
     data = Column(Unicode(255), nullable=False)
-
 
     user_id = Column(Integer, ForeignKey('tg_user.user_id'), index=True)
     user = relationship('User', uselist=False,
