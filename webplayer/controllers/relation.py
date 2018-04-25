d970046c307524cad867d25a7e91ec90ecc994aa
Dmitry Kruhlov <dmitriy.kruglov.work@gmail.com>
2018-04-25 16:11:49 +0300

    Table for raletion

--- webplayer/controllers/relation.py
+++ webplayer/controllers/relation.py
@@ -20,12 +20,18 @@ class RelationController(BaseController):
     def index(self, **kw):
         relations = DBSession.query(Relation).all()
         sounds = DBSession.query(Sound).all()
+        
+        relations_table = [[0 for i in range(0, len(sounds))] for j in range(0, len(sounds))]
+        for i in relations:
+            relations_table[i.left_sound_id-1][i.right_sound_id-1] = i.weight
+        
         return dict(page='relation',
-        	relations=relations)
+        	relations=relations,
+            relations_table=relations_table)
 
     @expose('json')
     def gener_new_relation(self):
     	sounds = DBSession.query(Sound).all()
     	sound_quantity = len(sounds)
-    	Relation.generation_new_relation(sound_quantity=sound_quantity)
+    	Relation.generation_new_relation(sound_quantity)
     	redirect('/relation')
